450213de39315a2f27482eec6e1d2e2e8fb7e9c9
v.ionov <v.ionov@mycrg.ru>
2025-04-18 11:46:11 +0300

    init

--- src/main/java/org/example/ShpDbfConverter.java
+++ src/main/java/org/example/ShpDbfConverter.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.linuxense.javadbf.DBFDataType;
@@ -15,42 +17,46 @@ import com.linuxense.javadbf.DBFWriter;
 public class ShpDbfConverter {
 
     /**
-     * Конвертирует все поля TIMESTAMP_DBASE7 ('@') в DATE ('D') формата YYYYMMDD,
-     * и читает/пишет строки в UTF-8.
-     *
-     * Исходник: parcels_new_1_df2a__7829Polygon.dbf
-     * Результат: output.dbf
+     * Заменяем все поля TIMESTAMP_DBASE7 ('@') на CHARACTER(8) с форматом ddMMyyyy.
+     * Чтение/запись в UTF-8.
      */
-    public static void convertTimestampToDate(String inputDbfPath,
-                                              String outputDbfPath) throws Exception {
-        // Заменили CP866 на UTF-8:
+    public static void convertTimestampToCharDate(String inputDbfPath,
+                                                  String outputDbfPath) throws Exception {
         Charset charset = Charset.forName("UTF-8");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        try (InputStream  inStream = new FileInputStream(inputDbfPath);
-             DBFReader    reader   = new DBFReader(inStream, charset);
-             FileOutputStream fos   = new FileOutputStream(outputDbfPath)) {
+        try (InputStream inStream = new FileInputStream(inputDbfPath);
+             DBFReader reader = new DBFReader(inStream, charset);
+             FileOutputStream fos = new FileOutputStream(outputDbfPath)) {
 
-            // 1) Новый список полей: TIMESTAMP_DBASE7 → DATE
+            // 1) Собираем описание полей
             List<DBFField> newFields = new ArrayList<>();
             for (int i = 0; i < reader.getFieldCount(); i++) {
-                DBFField oldField = reader.getField(i);
-                if (oldField.getType() == DBFDataType.TIMESTAMP_DBASE7) {
-                    DBFField df = new DBFField();
-                    df.setName(oldField.getName());
-                    df.setType(DBFDataType.DATE);
-                    newFields.add(df);
+                DBFField old = reader.getField(i);
+                if (old.getType() == DBFDataType.TIMESTAMP_DBASE7) {
+                    DBFField fld = new DBFField();
+                    fld.setName(old.getName());
+                    fld.setType(DBFDataType.CHARACTER);
+                    fld.setFieldLength(10);          // ddMMyyyy
+                    newFields.add(fld);
                 } else {
-                    newFields.add(oldField);
+                    newFields.add(old);
                 }
             }
 
-            // 2) Писатель с UTF-8 и обновлёнными полями
+            // 2) Создаём DBFWriter и задаём все поля
             DBFWriter writer = new DBFWriter(fos, charset);
             writer.setFields(newFields.toArray(new DBFField[0]));
 
-            // 3) Копируем записи: Date автоматически → YYYYMMDD
+            // 3) Копируем записи, переводя Date → "ddMMyyyy"
             Object[] row;
             while ((row = reader.nextRecord()) != null) {
+                for (int i = 0; i < row.length; i++) {
+                    if (reader.getField(i).getType() == DBFDataType.TIMESTAMP_DBASE7
+                            && row[i] instanceof Date) {
+                        row[i] = sdf.format((Date) row[i]);
+                    }
+                }
                 writer.addRecord(row);
             }
 
@@ -62,8 +68,8 @@ public class ShpDbfConverter {
         String inputPath  = "parcels_new_1_df2a__7829Polygon.dbf";
         String outputPath = "output.dbf";
 
-        System.out.println("Читаем из: " + inputPath + " в UTF-8");
-        convertTimestampToDate(inputPath, outputPath);
+        System.out.println("Читаем из:  " + inputPath + " (UTF-8)");
+        convertTimestampToCharDate(inputPath, outputPath);
         System.out.println("Готово, записано в: " + outputPath);
     }
 }
